8c704aae517db30ad97fa02c534552e9cd25b06d
makhabatabd <makhabat9898@gmail.com>
2022-08-05 10:43:19 +0600

    the first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Site
+
+## Run :
+
+```.bash
+ git clone https://github.com/makhabatabd/quiz.git
+
+    npm install
+```
+
+### After initialization the project - run:
+
+```.bash
+    npm run start
+```
+
+### or
+
+```.bash
+    npm run build
+```
+
+### to get (dist) folder, which containts all files to run site.
---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: ["@babel/polyfill", "./src/js/index.js"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+  },
+  resolve: {
+    extensions: [".js"],
+  },
+  performance: { hints: false },
+  devServer: {
+    port: 3000,
+    static: {
+      directory: path.join(__dirname, "src"),
+    },
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      filename: "index.html",
+      template: "src/pages/index.pug",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "./src/assets"),
+          to: path.resolve(__dirname, "dist/assets"),
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        loader: "@webdiscus/pug-loader",
+      },
+      {
+        test: /\.(s[ac]ss)$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(jpe?g|png)$/i,
+        type: "asset",
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.squooshMinify,
+          options: {
+            encodeOptions: {
+              mozjpeg: {
+                quality: 100,
+              },
+              webp: {
+                lossless: 1,
+              },
+              avif: {
+                cqLevel: 0,
+              },
+            },
+          },
+        },
+      }),
+    ],
+  },
+};
